f070b6c7c6dbd5dd5601ba665d7c835648c3b39a
deyvisgc <deivisgarcia@upeu.edu.pe>
2019-02-12 15:55:14 -0500

    Initial commit

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Melody Admin</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="{{asset('vendors/iconfonts/font-awesome/css/all.min.css')}}">
+    <link rel="stylesheet" href="{{asset('vendors/css/vendor.bundle.base.css')}}">
+    <link rel="stylesheet" href="{{asset('vendors/css/vendor.bundle.addons.css')}}">
+    <link rel="stylesheet" href="{{asset('css/jquery-ui.css')}}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@7.28.11/dist/sweetalert2.min.css">
+    <link href="https://cdn.datatables.net/1.10.15/css/jquery.dataTables.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+
+
+    <link rel="stylesheet" href="{{asset('css/estilos.css')}}">
+    <link rel="stylesheet" href="{{asset('css/modal.css')}}">
+    <!-- endinject -->
+</head>
+<body>
+<div class="container-scroller">
+    <!-- partial:partials/_navbar.html -->
+    <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row default-layout-navbar">
+        <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+            <a class="navbar-brand brand-logo" href=""><img src="images/logo.svg" alt="logo"/></a>
+            <a class="navbar-brand brand-logo-mini" href=""><img src="images/logo-mini.svg" alt="logo"/></a>
+        </div>
+        <div class="navbar-menu-wrapper d-flex align-items-stretch">
+            <ul class="navbar-nav navbar-nav-right">
+                <li class="nav-item nav-profile dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
+
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
+                        <a class="dropdown-item">
+                            <i class="fas fa-cog text-primary"></i>
+                            Settings
+                        </a>
+                        <div class="dropdown-divider"></div>
+
+                        @if(auth()->check())
+                            <a class="dropdown-item"{{ route('logout') }}  onclick="event.preventDefault();
+             document.getElementById('logout-form').submit();" > <i class="fas fa-power-off text-primary" style="text-align: center">Cerrar</i></a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        @endif
+                    </div>
+                </li>
+                <li class="nav-item nav-settings d-none d-lg-block">
+                    <a class="nav-link" href="#">
+                        <i class="fas fa-ellipsis-h"></i>
+                    </a>
+                </li>
+            </ul>
+            <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+                <span class="fas fa-bars"></span>
+            </button>
+        </div>
+    </nav>
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+
+        <div id="right-sidebar" class="settings-panel">
+            <i class="settings-close fa fa-times"></i>
+            <ul class="nav nav-tabs" id="setting-panel" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link active" id="todo-tab" data-toggle="tab" href="#todo-section" role="tab" aria-controls="todo-section" aria-expanded="true">TO DO LIST</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="chats-tab" data-toggle="tab" href="#chats-section" role="tab" aria-controls="chats-section">CHATS</a>
+                </li>
+            </ul>
+        </div>
+        <!-- partial -->
+        <!-- partial:partials/_sidebar.html -->
+        <nav class="sidebar sidebar-offcanvas" id="sidebar">
+            <ul class="nav">
+                <li class="nav-item nav-profile">
+                    <div class="nav-link">
+                        <div class="profile-image">
+                            <img src="images/faces/face5.jpg" alt="image"/>
+                        </div>
+                        <div class="profile-name">
+                            <p class="name">
+                                Welcome Jane
+                            </p>
+                            <p class="designation">
+                                Super Admin
+                            </p>
+                        </div>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index-2.html">
+                        <i class="fa fa-home menu-icon"></i>
+                        <span class="menu-title">Inicio</span>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="form-elements">
+                        <i class="fab fa-wpforms menu-icon"></i>
+                        <span class="menu-title">Panel de Administrador</span>
+                        <i class="menu-arrow"></i>
+                    </a>
+                    <div class="collapse" id="form-elements">
+                        <ul class="nav flex-column sub-menu">
+                            <li class="nav-item"><a class="nav-link" href="{{url('registrar')}}">Registrar Usuario</a></li>
+                            <li class="nav-item"><a class="nav-link" href="{{url('getUser')}}">Listar Usuarios</a></li>
+                        </ul>
+                    </div>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" data-toggle="collapse" href="#page-layouts" aria-expanded="false" aria-controls="page-layouts">
+                        <i class="fab fa-trello menu-icon"></i>
+                        <span class="menu-title">Mantenimiento</span>
+                        <i class="menu-arrow"></i>
+                    </a>
+                    <div class="collapse" id="page-layouts">
+                        <ul class="nav flex-column sub-menu">
+                            <li class="nav-item d-none d-lg-block"> <a class="nav-link" href="{{url('cliente')}}">Cliente</a></li>
+                            <li class="nav-item"> <a class="nav-link" href="{{url('habitacion')}}">Habitacion</a></li>
+                            <li class="nav-item d-none d-lg-block"> <a class="nav-link" href="{{url('Piso')}}">Niveles</a></li>
+                            <li class="nav-item"> <a class="nav-link" href="{{url('tipo_Cliente')}}">Tipo Habitacion</a></li>
+
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+        <!-- partial -->
+        <div class="main-panel">
+            <div class="content-wrapper">
+                <div class="page-header">
+                </div>
+                @yield('contenido')
+            </div>
+
+            <!-- content-wrapper ends -->
+            <!-- partial:partials/_footer.html -->
+            <footer class="footer">
+                <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                    <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2018 <a href="https://www.templatespoint.net/" target="_blank">TemplatesPoint</a>. All rights reserved.</span>
+                    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="far fa-heart text-danger"></i></span>
+                </div>
+            </footer>
+            <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+</div>
+<!-- container-scroller -->
+
+
+
+<!-- plugins:js -->
+<script src="{{asset('vendors/js/vendor.bundle.base.js')}}"></script>
+<script src="{{asset('vendors/js/vendor.bundle.addons.js')}}"></script>
+<!-- endinject -->
+<!-- Plugin js for this page-->
+<!-- End plugin js for this page-->
+<!-- inject:js -->
+<script src="{{asset('js/off-canvas.js')}}"></script>
+<script src="{{asset('js/hoverable-collapse.js')}}"></script>
+<script src="{{asset('js/misc.js')}}"></script>
+<script src="{{asset('js/settings.js')}}"></script>
+<script src="{{asset('js/todolist.js')}}"></script>
+
+<!-- endinject -->
+<!-- Custom js for this page-->
+<script src="{{asset('js/dashboard.js')}}"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.28.10/dist/sweetalert2.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js" type="text/javascript"></script>
+
+
+
+
+
+
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.print.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js" integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30=" crossorigin="anonymous"></script>
+
+
+<!-- Latest compiled JavaScript -->
+
+<!-- Minified Bootstrap JS -->
+
+<!-- endinject -->
+<!-- Custom js for this page-->
+<script>
+
+    function get_fhoy() {
+        // FORMATO DE HTML5 input[date] >> 'aaaa-mm-dd'
+        var texto = '';
+        var fecha = new Date();
+        var anho = fecha.getFullYear().toString();
+        var mes = ("0" + (fecha.getMonth() + 1)).slice(-2);
+        var dia = ("0" + fecha.getDate()).slice(-2);
+        texto = anho+'-'+mes+'-'+dia;
+        return texto;
+    }
+</script>
+@yield('script')
+
+<!-- End custom js for this page-->
+</body>
+
+
+</html>
