eddb4041aa1e4d5a4059bc0b89edbdd9b2b96cc6
Zannier7 <zanniervargas@upeu.edu.pe>
2019-02-13 10:52:16 -0500

    pagina web

---
+++ resources/views/web/serivicios/mecanicageneral.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Def-CON
+ * Date: 12-Feb-19
+ * Time: 17:19
+ */
---
+++ resources/views/web/venta/venta.blade.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="icon" href="img/fav-icon.png" type="image/x-icon" />
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Construction - WeBuilder Template</title>
+
+    <!-- Icon css link -->
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/materialdesignicons.min.css" rel="stylesheet">
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Rev slider css -->
+    <link href="vendors/revolution/css/settings.css" rel="stylesheet">
+    <link href="vendors/revolution/css/layers.css" rel="stylesheet">
+    <link href="vendors/revolution/css/navigation.css" rel="stylesheet">
+    <link href="vendors/animate-css/animate.css" rel="stylesheet">
+    <link href="vendors/owl-carousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Extra plugin css -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+
+<!--================Header Area =================-->
+<header class="main_header_area">
+    <div class="header_top_area">
+        <div class="container">
+            <div class="pull-left">
+                <a href="#"><i class="fa fa-phone"></i>(01) - 547 7201 / 980 610 635</a>
+                <a href="#"><i class="fa fa-map-marker"></i>Av. Calca 104 Coop. 27 de Abril-Ate</a>
+                <a href="#"><i class="mdi mdi-clock"></i>08 AM - 5 PM</a>
+            </div>
+            <div class="pull-right">
+                <ul class="header_social">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                    <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="main_menu_area">
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.html"><img src="img/logo.png" alt=""></a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="dropdown submenu active">
+                            <a href="#inicio" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Inicio</a>
+                        </li>
+                        <li class="dropdown submenu">
+                            <a href="#servicios" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Servicios <i class="mdi mdi-arrow-down-drop-circle-outline"></i></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="project-full-width.html">Laboratorio DIESEL</a></li>
+                                <li><a href="venta.html">Mecánica General <br>Livianos y Pesados</a></li>
+                                <li><a href="project-grid-two-column.html">Venta de Repuetos</a></li>
+                                <li><a href="project-three-column.html">Car Wash</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="service-single.html">Quiénes Somos?</a></li>
+                        <li><a href="#galeria">Galeria</a></li>
+                        <li class="dropdown submenu">
+                            <a href="blog-details.html" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Repuestos <i class="mdi mdi-arrow-down-drop-circle-outline"></i></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="venta.html">General</a></li>
+                                <li><a href="venta.html">Promociones</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#contactenos">Contactenos</a></li>
+                        <li class="icon_search"><a href="#"><i class="mdi mdi-cart"></i></a></li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </nav>
+        </div>
+    </div>
+</header>
+<!--================Header Area =================-->
+
+<!--================Banner Area =================-->
+<section class="banner_area">
+    <div class="container">
+        <div class="banner_inner_text">
+            <h4>Project full width</h4>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li class="active"><a href="#">Project full width 2 colum</a></li>
+            </ul>
+        </div>
+    </div>
+</section>
+<!--================End Banner Area =================-->
+
+<!--================Our Project2 Area =================-->
+<section class="our_project2_area project_grid_three">
+    <div class="container">
+        <div class="main_c_b_title">
+            <h2>VENTA<br class="title_br">REPUESTOS</h2>
+            <h6>Al mejor precio y de buena calidad</h6>
+        </div>
+        <ul class="our_project_filter">
+            <li class="active" data-filter="*"><a href="#">All</a></li>
+            <li data-filter=".building"><a href="#">Buildings</a></li>
+            <li data-filter=".interior"><a href="#">Interior</a></li>
+            <li data-filter=".design"><a href="#">Design</a></li>
+            <li data-filter=".isolation"><a href="#">Isolation</a></li>
+            <li data-filter=".plumbing"><a href="#">Plumbing</a></li>
+            <li data-filter=".tiling"><a href="#">Tiling</a></li>
+        </ul>
+        <div class="row our_project_details">
+            <div class="col-md-3 col-sm-12 col-xs-12 building isolation interior">
+                <div style="border: solid rgba(192,178,195,0.27) 1px; padding-top: 5px;border-radius: 5px;">
+                    <img style="margin-left: 5px;" class="img-responsive" src="img/project/project-grid-three/project-g-three-1.jpg" alt="no cargo la imagen" width="250" height="250">
+                    <div style="margin-top: 10px; background-color: #f6e9ff;padding: 15px 0px 15px 0px;">
+                        <div class="text-center">
+                            <a href="#"><h5 class="text-center" style="color:black; padding-bottom: 5px; height: 90px">Manguera hidroaulica idrocarnoasnd aosdnaosdn </h5></a>
+                            <p class=" text-center" style="padding-bottom: 10px; font-size: 20px;font-family: bold;color: red;">$ 250.00</p>
+                            <a class=" text-center btn btn-primary" href="#">Comprar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--================End Our Project2 Area =================-->
+
+<section class="address_area" id="contactenos">
+    <div class="container">
+        <div class="row address_inner">
+            <div class="col-md-4">
+                <div class="media">
+                    <div class="media-left">
+                        <img src="img/icon/place-icon.png" alt="">
+                    </div>
+                    <div class="media-body">
+                        <h4>Ubicanos en :</h4>
+                        <h5>Av. Calca 104 Coop. 27 de Abril   Ate</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="media">
+                    <div class="media-left">
+                        <img src="img/icon/phone-icon.png" alt="">
+                    </div>
+                    <div class="media-body">
+                        <h5>(01) 547 7201</h5>
+                        <h5>(+51) 980610 635</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="media">
+                    <div class="media-left">
+                        <img src="img/icon/inbox-icon.png" alt="">
+                    </div>
+                    <div class="media-body">
+                        <p>ocollantes@overhauldiesel.com.pe</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--================Footer Area =================-->
+<footer class="footer_area">
+    <!--<div class="footer_widgets_area">
+        <div class="container">
+            <div class="row footer_widgets_inner">
+                <div class="col-md-4 col-sm-6">
+                    <aside class="f_widget about_widget">
+                        <img src="img/footer-logo.png" alt="">
+                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusan-tium doloremque laudantium. ed quia consequuntur magni dolores eos qui ratione.</p>
+                        <ul>
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                            <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                        </ul>
+                    </aside>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <aside class="f_widget recent_widget">
+                        <div class="f_w_title">
+                            <h3>Recent Posts</h3>
+                        </div>
+                        <div class="recent_w_inner">
+                            <div class="media">
+                                <div class="media-left">
+                                    <img src="img/blog/recent-post/recent-1.png" alt="">
+                                </div>
+                                <div class="media-body">
+                                    <a href="#"><p>The road to success is always under con-struction</p></a>
+                                </div>
+                            </div>
+                            <div class="media">
+                                <div class="media-left">
+                                    <img src="img/blog/recent-post/recent-2.png" alt="">
+                                </div>
+                                <div class="media-body">
+                                    <a href="#"><p>The road to success is always under con-struction</p></a>
+                                </div>
+                            </div>
+                        </div>
+                    </aside>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <aside class="f_widget give_us_widget">
+                        <h5>Llamanos</h5>
+                        <h4>(01) 547 7201</h4>
+                        <h4>(+51) 980 610 635</h4>
+                    </aside>
+                </div>
+            </div>
+        </div>
+    </div>-->
+    <div class="footer_copy_right">
+        <div class="container">
+            <h4><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                Copyright &copy;<script>document.write(new Date().getFullYear());</script> Todos los derechos reservados | Realizado por <a href="https://gruposystem.com" target="_blank">Grupo System V&V</a>
+                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></h4>
+        </div>
+    </div>
+</footer>
+<!--================End Footer Area =================-->
+
+
+
+
+
+
+
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery-2.2.4.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+<!-- Rev slider js -->
+<script src="vendors/revolution/js/jquery.themepunch.tools.min.js"></script>
+<script src="vendors/revolution/js/jquery.themepunch.revolution.min.js"></script>
+<script src="vendors/revolution/js/extensions/revolution.extension.video.min.js"></script>
+<script src="vendors/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script src="vendors/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script src="vendors/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+
+<script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+<script src="vendors/isotope/imagesloaded.pkgd.min.js"></script>
+<script src="vendors/isotope/isotope.pkgd.min.js"></script>
+<script src="vendors/magnific-popup/jquery.magnific-popup.min.js"></script>
+<script src="vendors/counterup/waypoints.min.js"></script>
+<script src="vendors/counterup/jquery.counterup.min.js"></script>
+<script src="vendors/flex-slider/jquery.flexslider-min.js"></script>
+
+<!--gmaps Js-->
+<script src="js/gmaps.min.js"></script>
+
+<script src="js/theme.js"></script>
+</body>
+</html>
